2a0b4a70dabcb31bc213612cb7fbbe7f7ef5899d
mrngocthien <108567256+mrngocthien@users.noreply.github.com>
2023-03-19 10:26:40 +0700

    v2

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,9 +1,27 @@
-
+import {useState} from 'react'
 
 function App() {
+const [works, setWorks] = useState('')
+
+const handleChange = (event) => {
+  setWorks(event.target.value)
+  console.log(works)
+}
+
+
   return (
-    <div className="App">
-      app
+    <div className="flex gap-8 h-screen justify-center items-center border border-red-500">
+      <input 
+        type="text"
+        className="outline-none px-4 py-2 border border-blue-600 rounded-md w-[400px]"
+        onChange={handleChange} 
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
     </div>
   );
 }
