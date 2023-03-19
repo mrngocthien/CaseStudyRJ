369e11d232a073d765f181470af6f3422028129d
mrngocthien <108567256+mrngocthien@users.noreply.github.com>
2023-03-20 06:28:09 +0700

    up

---
+++ zingMp3/src/pages/public/Public.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import { SidebarLeft, SidebarRight } from '../../components';
+
+const Public = () => {
+  return (
+    <div className='w-full flex overflow-y-auto bg-slate-800 text-white'>
+        <div className='w-[240px] flex-none border border-blue-500'>
+            <SidebarLeft />
+        </div>
+        <div className='flex-auto border border-red-500'>
+            <Outlet />
+        </div>
+        <div className='w-[329px] flex-none border border-green-500'>
+            <SidebarRight />
+        </div>
+    </div>
+  )
+}
+
+export default Public
---
+++ zingMp3/src/ultis/icons.js
@@ -0,0 +1,11 @@
+import { MdOutlineLibraryMusic } from 'react-icons/md'
+import { GiChart} from 'react-icons/gi'
+import { BiDisc } from 'react-icons/bi'
+
+const icons = {
+    MdOutlineLibraryMusic,
+    GiChart,
+    BiDisc
+}
+
+export default icons;
